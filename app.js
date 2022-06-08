680db48ed5ec8c2059e6ef5b0658774e03239347
bugger69 <aatreya21@gmail.com>
2022-06-08 16:52:24 +0530

    almost done

--- app.js
+++ app.js
@@ -36,31 +36,47 @@ function precedence(c) {
     }
 }
 
-function solve(s) {
+function solve(s) {//fix this algorithm lol....
     let stack1 = [];
     let stack2 = [];
     let i = 0;
     let s1 = "";
     while(i != s.length){
+        console.log(s[i]);
         if (precedence(s[i]) === -1) {
+            console.log("a");
             s1 += s[i];
             i++;
-        } else if (precedence(s[i]) != - 1) {
-            stack1.push(parseFloat(s1));
+        } else {
+            console.log("b", s1);
+            if(s1 != "") stack1.push(parseFloat(s1));
             s1 = "";
-            if(precedence(s[i]) >= precedence(stack2[stack2.length - 1])){
+            if(stack2.length === 0 || (precedence(s[i]) > precedence(stack2[stack2.length - 1]))){
+                console.log("c");
                 stack2.push(s[i]);
                 i++;
-            }// else {
-            //     process();
-            // }
+            }else {
+                let k = process(stack1, stack2);//while loop
+                console.log("d", k);
+                stack1.pop();   
+                stack1.pop();
+                stack2.pop();
+                stack1.push(k);
+            }
         }
     }
     stack1.push(parseFloat(s1));
-    return stack1[stack1.length - 1];
+    // while(stack2.length != 0 || stack1.length != 0) {
+        let k = process(stack1, stack2);
+        stack1.pop();
+        stack1.pop();
+        stack2.pop();
+        stack1.push(k);
+    // }
+    return stack1[0];
 }
 
-function process() {//do make sure to pass by reference later on.
+function process(stack1, stack2) {//do make sure to pass by reference later on.
     let b = stack1[stack1.length - 1];
     stack1.pop();
     let a = stack1[stack1.length - 1];
@@ -68,15 +84,15 @@ function process() {//do make sure to pass by reference later on.
     let c = stack2[stack2.length - 1];
     stack2.pop();
     if(c === '+') {
-        stack1.push(a + b);
+        return (a + b);
     } else if (c === '-') {
-        stack1.push(a - b);
+        return (a - b);
     } else if (c === '*') {
-        stack1.push(a * b);
+        return (a * b);
     } else if (c === '/') {
-        stack1.push(a / b);
+        return (a / b);
     } else {
-        return;
+        return NaN;
     }
 }
 
